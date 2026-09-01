2ba30bf15d3b68f9ff18d65e2f3e543650093a6a
zakarea najjar <mr.zakarea1@gmail.com>
2026-09-01 22:09:40 +0300

    fixing bugs

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import '/public/styles/styles.scss';
 import 'normalize.css/normalize.css';
 
 
+  const label = document.querySelectorAll('label[for="select-all"]');
+  document.querySelectorAll('#select-all').forEach(selectAll => {
+    selectAll.addEventListener('change', () => {
+      document.querySelectorAll('#item-1').forEach(item => {
+        item.checked = selectAll.checked;
+      });
+    });
+  });
